165bd89f5a09ddaf259af59fdb40fcdc98d8329d
Tyler McAnally <tylerdmcanally@gmail.com>
2025-12-26 11:27:31 -0600

    fix: Firebase version mismatch in web-portal

--- web-portal/lib/firebase.ts
+++ web-portal/lib/firebase.ts
@@ -23,6 +23,8 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-configureFirestoreRealtime(Firestore);
+// Configure SDK realtime helpers - use type assertion due to minor Firebase version differences
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+configureFirestoreRealtime(Firestore as any);
 
 export default app;
